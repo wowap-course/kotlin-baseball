e7af56c1636cc02b5a7391780bc007067e8ef3f4
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-04-04 21:44:13 +0900

    feat : 플레이어는 컴퓨터가 생각하고 있는 서로 다른 3개의 숫자를 입력한다.

--- src/main/kotlin/baseball/domain/GameController.kt
+++ src/main/kotlin/baseball/domain/GameController.kt
@@ -8,13 +8,22 @@ import baseball.view.OutputView
 class GameController(
     private val inputView: InputView,
     private val outputView: OutputView
-){
-    fun run(){
-        val baseballNumber = inputView.inputBaseBallNumber()
+) {
+    fun run() {
+        outputView.gameStart()
+        start()
     }
 
-
-
-
+    private fun start(){
+        val randomNumberGenerator = RandomNumberGenerator()
+        val randomBaseballNumber: List<Int> = randomNumberGenerator.getNumber()
+        val baseballNumber = inputView.inputBaseBallNumber()
+        try {
+            require(baseballNumber is Int)
+            require(baseballNumber in 100..999)
+        } catch (e: IllegalArgumentException) {
+            return start()
+        }
+    }
 
 }
--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -2,9 +2,8 @@ package baseball.view
 
 class InputView {
 
-    fun inputBaseBallNumber(): Int {
+    fun inputBaseBallNumber(): Int? {
         print("숫자를 입력해주세요 : ")
-        return readln().toInt()
+        return readln().toIntOrNull()
     }
-
 }
