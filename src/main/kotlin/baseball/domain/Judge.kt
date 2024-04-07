761c67899f7bddebcad741c0803fecfb5425a0ce
SeongWooHong <pluto0303@naver.com>
2024-04-07 23:08:59 +0900

    refactor(Judge.kt): fix method

--- src/main/kotlin/baseball/domain/Judge.kt
+++ src/main/kotlin/baseball/domain/Judge.kt
@@ -2,7 +2,7 @@ package baseball.domain
 
 class Judge {
     fun calculateStrike(inputNum: List<Int>, answer: List<Int>): Int {
-        return inputNum.zip(answer).count { it.first == it.second }
+        return inputNum.indices.count { inputNum[it] == answer[it] }
     }
 
     fun calculateBall(inputNum: List<Int>, answer: List<Int>): Int {
