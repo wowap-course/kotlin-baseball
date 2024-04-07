25743b71dc36ae7e3dc57bf6f003617eec3e5f86
SeongWooHong <pluto0303@naver.com>
2024-04-07 15:43:36 +0900

    Test(RandomNumberGeneratorTest.kt): add Test Code of fun randomNumGenerate

--- src/test/kotlin/baseball/RandomNumberGeneratorTest.kt
+++ src/test/kotlin/baseball/RandomNumberGeneratorTest.kt
@@ -21,4 +21,15 @@ class RandomNumberGeneratorTest {
         assertThat(result).isFalse()
     }
 
+    @Test
+    fun `randomNumGenerate 랜덤 수 3개를 반환한다`() {
+        val numbers = RandomNumberGenerator().numberGenerate()
+        assertThat(numbers.size).isEqualTo(3)
+    }
+
+    @Test
+    fun `randomNumberGenerate 랜덤 수는 겹치지 않는 3개의 수이다`() {
+        val numbers = RandomNumberGenerator().numberGenerate().toSet()
+        assertThat(numbers.size).isEqualTo(3)
+    }
 }
