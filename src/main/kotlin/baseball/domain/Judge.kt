3553ac4e8f2463eccc96575775ffc390cdc4dcc9
SeongWooHong <pluto0303@naver.com>
2024-04-07 17:19:33 +0900

    feat(Judge.kt): add fun calculateStrike & calculateBall

--- src/main/kotlin/baseball/domain/Judge.kt
+++ src/main/kotlin/baseball/domain/Judge.kt
@@ -1,4 +1,11 @@
 package baseball.domain
 
 class Judge {
+    fun calculateStrike(inputNum: List<Int>, answer: List<Int>): Int {
+        return inputNum.zip(answer).count { it.first == it.second }
+    }
+
+    fun calculateBall(inputNum: List<Int>, answer: List<Int>): Int {
+        return inputNum.count { it in answer && it != answer[inputNum.indexOf(it)] }
+    }
 }
--- src/test/kotlin/baseball/JudgeTest.kt
+++ src/test/kotlin/baseball/JudgeTest.kt
@@ -1,6 +1,25 @@
 package baseball
 
+import baseball.domain.Judge
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class JudgeTest {
+    @Test
+    fun `calculateStrike 스트라이크 개수를 세어준다`() {
+        val inputNumber = listOf(4, 5, 1)
+        val randomNumber = listOf(7, 5, 4)
+
+        val result = Judge().calculateStrike(inputNumber, randomNumber)
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    fun `calculateBall 볼 개수를 세어준다`() {
+        val inputNumber = listOf(4, 5, 1)
+        val randomNumber = listOf(7, 5, 4)
+
+        val result = Judge().calculateBall(inputNumber, randomNumber)
+        assertThat(result).isEqualTo(1)
+    }
 }
