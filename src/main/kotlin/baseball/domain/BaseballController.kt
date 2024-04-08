c7560c1664fe418c619d59831ce53356e603558b
SeongWooHong <pluto0303@naver.com>
2024-04-08 18:46:00 +0900

    feat(BaseBallController.kt): add fun baseballStart

--- src/main/kotlin/baseball/domain/BaseballController.kt
+++ src/main/kotlin/baseball/domain/BaseballController.kt
@@ -13,4 +13,11 @@ class BaseballController {
         println("숫자 야구 게임을 시작합니다.")
         baseballStart()
     }
+
+    private fun baseballStart() {
+        baseballPlay(RandomNumberGenerator().randomNumberGenerate())
+        decideMoreOrStop()
+    }
+
+
 }
