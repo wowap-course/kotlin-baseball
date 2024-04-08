945a19e17835f0781a3e5ae780a33d29496cd0d6
김민석|Minseok Kim <alstjr7437@naver.com>
2024-04-08 22:42:31 +0900

    feat: error 출력 구현 및 게임 출력 변경
    게임 출력을
    맞추지 못했을때와
    맞췄을때를 나눠서 print하도록 변경

--- BaseBall/BaseBall/view/OutputView.swift
+++ BaseBall/BaseBall/view/OutputView.swift
@@ -15,8 +15,6 @@ class OutputView{
     func nowScorePrint(result: [Int]){
         if result.first == 1 {
             print("낫싱")
-        } else if result.last == 3 {
-            print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
         } else if result[1] > 0 && result[2] > 0 {
             print("\(result[1])볼 \(result[2])스트라이크")
         } else if result[1] > 0 {
@@ -25,9 +23,16 @@ class OutputView{
             print("\(result[2])스트라이크")
         }
     }
-
+    
+    func resultGamePrint(){
+        print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    }
+    
     func endGamePrint(){
-        print("게임이 종료되었습니다!!")
-        
+        print("전체 게임이 종료되었습니다!!")
+    }
+    
+    func errorInputPrint(){
+        print("올바른 숫자 양식이 아닙니다!")
     }
 }
