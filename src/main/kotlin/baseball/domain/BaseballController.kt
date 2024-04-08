d28993f3de303c07f228a0b0c9ba865100d8a5b4
SeongWooHong <pluto0303@naver.com>
2024-04-08 18:45:40 +0900

    feat(BaseBallController.kt): add inputView, outputView, Judge Instance

--- src/main/kotlin/baseball/domain/BaseballController.kt
+++ src/main/kotlin/baseball/domain/BaseballController.kt
@@ -5,6 +5,9 @@ import baseball.view.InputView
 import baseball.view.OutputView
 
 class BaseballController {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+    private val judge = Judge()
 
     fun run() {
         println("숫자 야구 게임을 시작합니다.")
