9ced45acb6a86bc4a7913ebd0a51c3fa8c4d3870
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-08 23:06:16 +0900

    feat: 게임에 대한 출력을 다루는 OutputView 생성

---
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -0,0 +1,10 @@
+package baseball.view
+
+class OutputView {
+    fun printResultOfInning(result : String){
+        println(result)
+    }
+    fun printResultOfBaseBall(){
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    }
+}
