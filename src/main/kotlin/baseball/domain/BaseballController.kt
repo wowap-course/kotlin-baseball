a2975a2c436f6d440e6499364b5cb478e492ecdc
SeongWooHong <pluto0303@naver.com>
2024-04-08 18:44:49 +0900

    feat(BaseBallController.kt): add run fun & inputView, outputView, judge instance

--- src/main/kotlin/baseball/domain/BaseballController.kt
+++ src/main/kotlin/baseball/domain/BaseballController.kt
@@ -1,4 +1,13 @@
 package baseball.domain
 
+
+import baseball.view.InputView
+import baseball.view.OutputView
+
 class BaseballController {
+
+    fun run() {
+        println("숫자 야구 게임을 시작합니다.")
+        baseballStart()
+    }
 }
