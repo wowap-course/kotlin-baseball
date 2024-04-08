1f9181d32f28ac7955d8e6e2a64e156d650ba21a
SeongWooHong <pluto0303@naver.com>
2024-04-08 17:51:14 +0900

    feat(OutputView.kt): add output method

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -1,4 +1,19 @@
 package baseball.view
 
 class OutputView {
+    fun printNothing() {
+        println("낫싱")
+    }
+
+    fun printStrikeOnly(strike: Int) {
+        println("${strike}스트라이크")
+    }
+
+    fun printBallOnly(ball : Int) {
+        println("${ball}볼")
+    }
+
+    fun printBallAndStrike(strike: Int, ball: Int) {
+        println("${ball}볼 ${strike}스트라이크")
+    }
 }
