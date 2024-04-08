da808873a6bb2c64331bfa3fd621db144e7b5854
SeongWooHong <pluto0303@naver.com>
2024-04-08 18:43:31 +0900

    feat(InputView.kt): add fun inputNumber, printDecideMoreOrStop

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,4 +1,13 @@
 package baseball.view
 
 class InputView {
+    fun inputNumber() : Int? {
+        print("숫자를 입력해주세요 : ")
+        return readln().toIntOrNull()
+    }
+
+    fun printDecideMoreOrStop(): Int? {
+        print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        return readln().toIntOrNull()
+    }
 }
