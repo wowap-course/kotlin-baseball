00f55592f0aefd38f5036cbf425b34beec909c4c
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-08 22:19:43 +0900

    style: 가독성을 위해 숫자를 MAX_NUMBER_LENGTH로 변경

--- src/main/kotlin/baseball/domain/numbergenerator/RandomNumberGenerator.kt
+++ src/main/kotlin/baseball/domain/numbergenerator/RandomNumberGenerator.kt
@@ -5,7 +5,7 @@ import baseball.domain.digitsrule.DigitsRule
 class RandomNumberGenerator : NumberGenerator  {
     override fun get(): String {
         val number = mutableSetOf<Int>()
-        while(number.size < 4) {
+        while(number.size < MAX_NUMBER_LENGTH) {
             number.add((MIN_RANDOM_NUMBER..MAX_RANDOM_NUMBER).random())
         }
         return number.toString()
@@ -14,5 +14,6 @@ class RandomNumberGenerator : NumberGenerator  {
     companion object {
         private const val MIN_RANDOM_NUMBER = 1
         private const val MAX_RANDOM_NUMBER = 9
+        private const val MAX_NUMBER_LENGTH = 4
     }
 }
