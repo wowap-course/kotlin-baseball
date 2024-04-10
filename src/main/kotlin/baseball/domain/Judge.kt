721bf8083571bcb7d0408897dbf626ae91d23764
SeongWooHong <pluto0303@naver.com>
2024-04-10 17:46:04 +0900

    refactor(Judge.kt): List형인 변수 이름 복수형으로 변경

--- src/main/kotlin/baseball/domain/Judge.kt
+++ src/main/kotlin/baseball/domain/Judge.kt
@@ -1,11 +1,11 @@
 package baseball.domain
 
 class Judge {
-    fun calculateStrike(number1: List<Int>, number2: List<Int>): Int {
-        return number1.indices.count { number1[it] == number2[it] }
+    fun calculateStrike(numbers1: List<Int>, numbers2: List<Int>): Int {
+        return numbers1.indices.count { numbers1[it] == numbers2[it] }
     }
 
-    fun calculateBall(number1: List<Int>, number2: List<Int>): Int {
-        return number1.count { it in number2 && it != number2[number1.indexOf(it)] }
+    fun calculateBall(numbers1: List<Int>, numbers2: List<Int>): Int {
+        return numbers1.count { it in numbers2 && it != numbers2[numbers1.indexOf(it)] }
     }
 }
