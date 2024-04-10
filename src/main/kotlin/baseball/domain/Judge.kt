6ab313169035b49e22be079e9c60c4bcdb8be731
SeongWooHong <pluto0303@naver.com>
2024-04-10 17:41:57 +0900

    refactor(Judge.kt): Judge가 알 필요 없는 변수 명 수정

--- src/main/kotlin/baseball/domain/Judge.kt
+++ src/main/kotlin/baseball/domain/Judge.kt
@@ -1,11 +1,11 @@
 package baseball.domain
 
 class Judge {
-    fun calculateStrike(inputNum: List<Int>, answer: List<Int>): Int {
-        return inputNum.indices.count { inputNum[it] == answer[it] }
+    fun calculateStrike(number1: List<Int>, number2: List<Int>): Int {
+        return number1.indices.count { number1[it] == number2[it] }
     }
 
-    fun calculateBall(inputNum: List<Int>, answer: List<Int>): Int {
-        return inputNum.count { it in answer && it != answer[inputNum.indexOf(it)] }
+    fun calculateBall(number1: List<Int>, number2: List<Int>): Int {
+        return number1.count { it in number2 && it != number2[number1.indexOf(it)] }
     }
 }
