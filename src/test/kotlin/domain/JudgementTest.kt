86b74faa7a42b2904934cdae2003f529795d727e
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-04-12 22:37:09 +0900

    Test : 기능의 요구사항에 맞게 테스트 세분화 + 테스트명 변경

--- src/test/kotlin/domain/JudgementTest.kt
+++ src/test/kotlin/domain/JudgementTest.kt
@@ -8,10 +8,18 @@ import org.junit.jupiter.api.Test
 class JudgementTest {
 
     @Test
-    fun `스트라이크, 볼, 나싱의 힌트를 표시한다`(){
+    fun `두 숫자를 비교했을 때, 값은 같고 자리만 다른 숫자가 하나라면 볼의 개수는 1 이다`(){
         val answerNumbers:List<Int> = listOf(4,5,6)
-        val targetNumbers:List<Int> = listOf(5,1,6)
+        val targetNumbers:List<Int> = listOf(5,1,2)
         val count = Judgement().correctCount(answerNumbers, targetNumbers)
-        assertThat(count).isEqualTo(Referee(1,1))
+        assertThat(count).isEqualTo(Referee(0,1))
+    }
+
+    @Test
+    fun `두 숫자를 비교했을 때, 값은 같고 자리도 같다면 스트라이크의 개수는 1 이다`(){
+        val answerNumbers:List<Int> = listOf(4,5,6)
+        val targetNumbers:List<Int> = listOf(3,1,6)
+        val count = Judgement().correctCount(answerNumbers, targetNumbers)
+        assertThat(count).isEqualTo(Referee(1,0))
     }
 }
