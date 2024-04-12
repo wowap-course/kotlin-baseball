989d8ff3dacffa293be55c1a891d5e6cf9f0be8a
김민석|Minseok Kim <alstjr7437@naver.com>
2024-04-12 15:18:15 +0900

    refactor: 랜덤 넘버 생성시 let으로 변경 후 shuffled로 변경
    var -> let으로 변경하여서 중간에 값이 바뀌는 부분을 방지함

--- BaseBall/BaseBall/domain/NumberGenerator/RandomNumberGenerator.swift
+++ BaseBall/BaseBall/domain/NumberGenerator/RandomNumberGenerator.swift
@@ -9,11 +9,9 @@ import Foundation
 
 class RandomNumberGenerator : NumberGenerator{
     
-    lazy var nums = Array(MIN_RANDOM_NUM...MAX_RANDOM_NUM)
-    
     func getNumbers() -> [Int] {
-        nums.shuffle()
-        return Array(nums.prefix(RANDOM_NUM_LENGTH))
+        let nums = Array(MIN_RANDOM_NUM...MAX_RANDOM_NUM)
+        return Array(nums.shuffled().prefix(RANDOM_NUM_LENGTH))
     }
     
     private let MIN_RANDOM_NUM = 1
