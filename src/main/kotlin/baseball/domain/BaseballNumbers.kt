d617087afe050745adb693178bb9ffecc6894c2d
vrexpert <vrexpert2302@gmail.com>
2024-04-12 21:17:27 +0900

    feat(BaseballNumbers): 숫자야구 숫자들 일급 컬렉션 구현 및 테스트

--- src/main/kotlin/baseball/domain/BaseballNumbers.kt
+++ src/main/kotlin/baseball/domain/BaseballNumbers.kt
@@ -13,9 +13,17 @@ class BaseballNumbers(numbers: Set<BaseballNumber>) {
         private const val ERROR_DUPLICATE = "숫자야구 숫자는 중복될 수 없습니다"
         private const val ERROR_NOT_VALID_SIZE = "숫자야구의 숫자 개수는 3개입니다"
 
+        @JvmName("fromBaseballNumbers")
         fun from(numbers: List<BaseballNumber>): BaseballNumbers {
             require(numbers.toSet().size == numbers.size) { ERROR_DUPLICATE }
             return BaseballNumbers(numbers = numbers.toSet())
+        }
+
+        @JvmName("fromInts")
+        fun from(numbers: List<Int>): BaseballNumbers {
+            val wrappedNumbers = numbers.map { BaseballNumber(it) }
+            require(numbers.toSet().size == numbers.size) { ERROR_DUPLICATE }
+            return BaseballNumbers(wrappedNumbers.toSet())
         }
     }
 }
--- src/test/kotlin/baseball/domain/BaseballNumbersTest.kt
+++ src/test/kotlin/baseball/domain/BaseballNumbersTest.kt
@@ -38,6 +38,20 @@ class BaseballNumbersTest {
     }
 
     @Test
+    fun `숫자야구 숫자들의 초기값을 정해주면 그 값을 읽어올 수 있다_3`() {
+        // given
+        val baseballNumbers = BaseballNumbers.from(listOf(1, 2, 3))
+        // when
+
+        // then
+        assertThat(baseballNumbers.numbers).containsExactly(
+            BaseballNumber(1),
+            BaseballNumber(2),
+            BaseballNumber(3),
+        )
+    }
+
+    @Test
     fun `숫자야구 숫자들은 중복될 수 없다`() {
         assertThrows<IllegalArgumentException> {
             BaseballNumbers.from(
