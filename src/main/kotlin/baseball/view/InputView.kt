25abc673fc9a47a0e8ae55d07cf88bfc5cf4cdb8
vrexpert <vrexpert2302@gmail.com>
2024-04-12 23:04:04 +0900

    feat(InputView): 사용자가 잘못 입력하면 오류 메세지를 보여준다

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -1,20 +1,26 @@
 package baseball.view
 
 class InputView {
-    fun readGuessedNumber(): Int {
+    fun readGuessedNumber(): List<Int> {
         return runCatching {
             print("숫자를 입력해주세요 : ")
-            requireNotNull(readln().toIntOrNull()) { "숫자를 입력해야합니다" }
-        }.getOrElse { readGuessedNumber() }
+            return readln().chunked(1).map { it.toIntOrNull() ?: throw IllegalArgumentException("숫자만 입력해주세요") }
+        }.getOrElse {
+            println("[ERROR] ${it.message}")
+            readGuessedNumber()
+        }
     }
 
-    fun readDecideMoreOrStop(): Int {
+    fun isContinue(): Boolean {
         return runCatching {
             print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
             val input = readln().toIntOrNull()
             requireNotNull(input) { "숫자를 입력해야합니다" }
             require(input in 1..2) { "1 또는 2를 입력해아합니다" }
-            input
-        }.getOrElse { readDecideMoreOrStop() }
+            input == 1
+        }.getOrElse {
+            println("[ERROR] ${it.message}")
+            isContinue()
+        }
     }
 }
