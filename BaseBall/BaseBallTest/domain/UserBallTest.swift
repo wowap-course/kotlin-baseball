ad4ace890fa9a91255321f62b4da0169c76a2094
김민석|Minseok Kim <alstjr7437@naver.com>
2024-04-16 01:43:42 +0900

    test: 에러 반환 함수 Given, Then 수정

--- BaseBall/BaseBallTest/domain/UserBallTest.swift
+++ BaseBall/BaseBallTest/domain/UserBallTest.swift
@@ -21,15 +21,27 @@ final class UserBallTest: XCTestCase {
     }
   
     func test_3자리의_수가_들어와야_한다() throws {
-        XCTAssertThrowsError(try UserBall(numbers: [1,2,3,4]))
+        // Given
+        let numbers = [1, 2, 3, 4]
+        
+        // Then
+        XCTAssertThrowsError(try UserBall(numbers: numbers))
     }
     
     func test_숫자는_같은_수가_입력되면_안된다() throws {
-        XCTAssertThrowsError(try UserBall(numbers: [3,3,1]))
+        // Given
+        let numbers = [3, 3, 1]
+        
+        // Then
+        XCTAssertThrowsError(try UserBall(numbers: numbers))
     }
     
     func test_숫자는_0은_불가능하다() throws {
-        XCTAssertThrowsError(try UserBall(numbers: [3,2,0]))
+        // Given
+        let numbers = [3, 2, 0]
+        
+        // Then
+        XCTAssertThrowsError(try UserBall(numbers: numbers))
     }
 
 }
