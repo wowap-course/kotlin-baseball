df528a7b5e456c5a1b56b5079d886f4d478c2f56
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-05-06 18:20:36 +0900

    refactor(InputView): 숫자만 입력받는다.

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -5,7 +5,7 @@ class InputView {
         print("숫자를 입력해주세요 : ")
         return runCatching {
             val answer = readln()
-            return validateInput(answer)
+            return numbersRead(answer)
         }.onFailure { e ->
             println(e.message)
         }.getOrElse { readAnswer() }
@@ -14,20 +14,8 @@ class InputView {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         return readln().toInt()
     }
-    private fun validateInput(answer : String) : List<Int> {
-        requireNumber(answer)
-        requireThreeDigits(answer)
-        requireNoDuplicate(answer)
-        return validate(answer)
+    private fun numbersRead(answer : String) : List<Int> {
+        require(answer.toIntOrNull() != null) {"[ERROR]: 입력된 값은 숫자가 아닙니다."}
+        return answer.toList().map { it.toString().toInt() }
     }
-    private fun requireNumber(inputNumber : String) {
-        require(inputNumber.toIntOrNull() != null) {"[ERROR]: 입력된 값은 숫자가 아닙니다."}
-    }
-    private fun requireThreeDigits(inputNumber: String) {
-        require(inputNumber.length == 3) {"[ERROR]: 입력된 값은 세자리가 아닙니다."}
-    }
-    private fun requireNoDuplicate(inputNumber: String) {
-        require(inputNumber.toSet().size == inputNumber.length) {"[ERROR]: 중복된 숫자가 있습니다."}
-    }
-    private fun validate(answer : String) : List<Int> = answer.toList().map { it.toString().toInt() }
 }
